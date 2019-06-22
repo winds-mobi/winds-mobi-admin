5f221f62bcb1266e5882a9beb63a475cd7d14b99
Yann Savary <yann.savary@gmail.com>
2019-06-22 17:03:23 +0200

    Forgot secret_key environ

--- winds_mobi_admin/settings.py
+++ winds_mobi_admin/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dev'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() not in ('false', 'no', '0')
