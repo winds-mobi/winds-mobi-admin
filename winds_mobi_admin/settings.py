2bef1c8d335161e00a26165fe2087929446f3ff9
Yann Savary <yann.savary@gmail.com>
2019-07-03 08:59:35 +0200

    Added env for STATIC_URL

--- winds_mobi_admin/settings.py
+++ winds_mobi_admin/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.environ.get('STATIC_URL') or '/static/'
 
 
 # winds.mobi settings
