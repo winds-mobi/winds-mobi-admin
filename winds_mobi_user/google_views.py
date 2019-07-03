f49259666bfd13818642d7e68a56b3c2edbc45f8
Yann Savary <yann.savary@gmail.com>
2019-07-03 22:59:11 +0200

    Using a file

--- winds_mobi_user/google_views.py
+++ winds_mobi_user/google_views.py
@@ -11,26 +11,27 @@ from .views import Oauth2Callback
 
 class GoogleOauth2Callback(Oauth2Callback):
     def get(self, request, *args, **kwargs):
-        client_secrets = json.load(settings.GOOGLE_CLIENT_SECRETS)
-        flow = Flow.from_client_config(
-            client_secrets,
-            scopes=['profile', 'email'],
-            redirect_uri=reverse('user:google_oauth2callback', request=self.request))
+        with open(settings.GOOGLE_CLIENT_SECRETS) as config_file:
+            client_secrets = json.load(config_file)
+            flow = Flow.from_client_config(
+                client_secrets,
+                scopes=['profile', 'email'],
+                redirect_uri=reverse('user:google_oauth2callback', request=self.request))
 
-        if 'code' not in self.request.GET:
-            url, state = flow.authorization_url()
-            return HttpResponseRedirect(url)
-        else:
-            auth_code = self.request.GET['code']
-            token = flow.fetch_token(code=auth_code)
-            user_info = requests.get('https://www.googleapis.com/oauth2/v3/userinfo',
-                                     params={'access_token': token.access_token}).json()
-            username = f"google-{user_info['sub']}"
-            email = user_info['email'] or ''
+            if 'code' not in self.request.GET:
+                url, state = flow.authorization_url()
+                return HttpResponseRedirect(url)
+            else:
+                auth_code = self.request.GET['code']
+                token = flow.fetch_token(code=auth_code)
+                user_info = requests.get('https://www.googleapis.com/oauth2/v3/userinfo',
+                                         params={'access_token': token.access_token}).json()
+                username = f"google-{user_info['sub']}"
+                email = user_info['email'] or ''
 
-            ott = self.save_user(username, email, user_info)
-            context = {
-                'ott': ott,
-                'redirect_url': '/stations/list'
-            }
-            return self.render_to_response(context)
+                ott = self.save_user(username, email, user_info)
+                context = {
+                    'ott': ott,
+                    'redirect_url': '/stations/list'
+                }
+                return self.render_to_response(context)
